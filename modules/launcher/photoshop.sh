3a8f323fffebe3fcc5d41ea3c94971fb3b282b9b
gmanka <gmankab@gmail.com>
2025-01-09 22:23:49 +0400

    [+] vkd3d

--- modules/launcher/photoshop.sh
+++ modules/launcher/photoshop.sh
@@ -13,13 +13,13 @@ if [ ! -d "$WINEPREFIX" ]; then
     mkdir -p $WINEPREFIX
     wine64 wineboot
     mkdir -p "$WINEPREFIX/drive_c/Program Files (x86)/Common Files"
-    mkdir -p "$WINEPREFIX/drive_c/users/$USER/AppData/Roaming/Adobe/Adobe Photoshop 2022"
     cp -r "$PHOTOSHOP_ROOT/Adobe" "$WINEPREFIX/drive_c/Program Files (x86)/Common Files"
-    cp -r "$PHOTOSHOP_ROOT/Adobe Photoshop 2022 Settings" "$WINEPREFIX/drive_c/users/$USER/AppData/Roaming/Adobe/Adobe Photoshop 2022/Adobe Photoshop 2022 Settings"
     ln -s "$WINEPREFIX/drive_c/windows/regedit.exe" "$WINEPREFIX/drive_c/windows/syswow64/regedit.exe"
+    wineserver -w
+    /app/vkd3d/setup_vkd3d_proton.sh install
 fi
 if [ -n "$vd" ]; then
     winetricks vd="$vd"
 fi
-wine64 $PHOTOSHOP_ROOT/photoshop.exe
+wine64 $PHOTOSHOP_ROOT/photoshop2022/photoshop.exe
 
